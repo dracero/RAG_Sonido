00509315660a2f0ee33cbe8a4b8aea73ed9e9e02
dracero <dracero@fi.uba.ar>
2025-11-24 16:44:46 -0300

    data_v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+
 
 
 
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [basicSsl()],
+    plugins: [],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
